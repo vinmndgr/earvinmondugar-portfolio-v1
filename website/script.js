cdf9beeb8f52f1b155e8adc24717a785549a177e
Earvin Mondugar <vinmondugar@protonmail.com>
2025-02-16 11:50:25 +0000

    version 2, with new color scheme and bootstrap

--- website/script.js
+++ website/script.js
@@ -1,14 +1,3 @@
 function scrollToSection(id) {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  }
-
-function topFunction() {
-    window.scrollTo(0, 0);
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.scrollIntoView({ behavior: "smooth" });
-  }
-  
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
